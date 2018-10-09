dbbcfb13d5d17c9a1656fce7ddf7911a3821e540
Jensen Judkins <thesmelliestman@gmail.com>
2018-10-09 08:44:29 -0600

    Created moddel folder and colortool file

---
+++ FirstiOSApp/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  FirstiOSApp
+//
+//  Created by Judkins, Jensen on 10/9/18.
+//  Copyright © 2018 Judkins, Jensen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}
