788d29704283017dc0e554751d85f6f8a4f32d79
Jensen Judkins <thesmelliestman@gmail.com>
2018-10-09 07:52:33 -0600

    Modified GitIgnore

---
+++ FirstiOSApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstiOSApp
+//
+//  Created by Judkins, Jensen on 10/9/18.
+//  Copyright © 2018 Judkins, Jensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
